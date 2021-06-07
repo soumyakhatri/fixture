a24da4dfdf16613ba0671220d3412b99edbb3e8f
soumyakhatri <soumya91rias@gmail.com>
2021-06-07 16:58:09 +0530

    update menu for mobile

--- JS/dashboard.js
+++ JS/dashboard.js
@@ -2,17 +2,10 @@ let menu = document.querySelector('.menu');
 let burger =  document.querySelector('.burger');
 
 burger.onclick = somu;
-// burger.onmouseover = show;
-// burger.onmouseout = hide;
+
 
 function somu(){
     menu.classList.toggle('animate');
 }
 
-// function show(){
-//     menu.classList.add('animate');
-// }
 
-// function hide(){
-//     menu.classList.remove('animate');
-// }
